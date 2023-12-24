7fbe2576ec2b903dff014849b3e8756bba82ecaf
tmanojcox <tmanojpop70@gmail.com>
2023-12-24 11:02:36 -0800

    Edit and remove funtion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import {PencilIcon} from "@heroicons/react/24/solid";
+function App () {
+  const [InputKaValue, setInputKaValue] = useState("");
+  const [arrayvalue, setarrayvalue] = useState([]);
+  const handlelist = () => {
+    
+    setarrayvalue([...arrayvalue,InputKaValue]);
+    setInputKaValue(""); // Clear the input after adding the item to the list
+    
+  };
+ 
+  const removeTodo = (index) => {
+    
+    const newarrayvalue = [...arrayvalue];
+    newarrayvalue.splice(index, 1);
+    setarrayvalue(newarrayvalue);
+  };
+  const EditTodo= (index) => {
+    const newarrayvalue = [...arrayvalue];
+    newarrayvalue[index] = InputKaValue;
+  
+    setarrayvalue (newarrayvalue)
+    setInputKaValue("")
+  };
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="border p-5">
+      <input
+        value={InputKaValue}
+        onKeyDown={(e)=>e.key === "Enter" && handlelist()}
+        onChange={(Event) => setInputKaValue(Event.target.value)}
+        type="text"
+        className="border rounded-md"
+        placeholder="Type your items????"
+      />
+      <button
+        onClick={()=> handlelist("")}
+        className="border bg-gray-400 px-1 ml-1 rounded-md"
+        
+      >
+        Submit
+      </button>
+      <ul>
+      {arrayvalue.map((InputKaValue, index) => (
+        <li key={index}>{InputKaValue}
+         <button className="border p-1 mx-2 rounded-2xl bg-gray-500  " onClick={()=>removeTodo(index)} >Remove</button>
+         <button className="border p-1 mx-2 rounded-2xl bg-gray-500  " onClick={()=>EditTodo(index)} ><PencilIcon className="h-3 w-10" /></button>
+          
+        </li>
+
+      ))}
+      
+      </ul>
     </div>
   );
 }
